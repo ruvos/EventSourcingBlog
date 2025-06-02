9aa4ce1875d4c04fdf13597038a08a30717aa4a6
Ruwen <ruwen.katschek@gmx.de>
2025-06-02 19:50:30 +0200

    feat: Make events re- and storable

--- src/DomainObject/Post/Event/PostCreatedEvent.php
+++ src/DomainObject/Post/Event/PostCreatedEvent.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ruvos\Blog\DomainObject\Post\Event;
 
+use DateTimeImmutable;
 use Ruvos\Blog\DomainObject\AbstractEvent;
 use Ruvos\Blog\DomainObject\Event;
 use Ruvos\Blog\Utils\DefaultDateTimeProvider;
@@ -33,5 +34,21 @@ final readonly class PostCreatedEvent extends AbstractEvent
         $eventId = DefaultUuidProvider::v4();
 
         return new self($eventId, $correlationId, $createdAt, $title);
+    }
+
+    public static function fromJson(string $payload)
+    {
+        $data = json_decode($payload, true, 512, JSON_THROW_ON_ERROR);
+        return new self(
+            $data['eventId'],
+            $data['correlationId'],
+            new DateTimeImmutable($data['createdAt']['date'], new \DateTimeZone($data['createdAt']['timezone'])),
+            $data['title']
+        );
+    }
+
+    public function toJson(): string
+    {
+        return json_encode($this, JSON_THROW_ON_ERROR);
     }
 }
