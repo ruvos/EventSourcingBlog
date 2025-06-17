7ac2d5e1e1606789ac5464e094564a60e8b25aba
Ruwen Katschek <ruwen@acer-laptop-ruwe.ruwen>
2025-06-17 14:32:29 +0200

    test: Add test to load all Posts

--- tests/Unit/Post/PostRepositoryTest.php
+++ tests/Unit/Post/PostRepositoryTest.php
@@ -3,28 +3,51 @@
 namespace Blog\Tests\Unit\Post;
 
 use Blog\Tests\Unit\Post\Mock\EventStoreMock;
+use DateTimeImmutable;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\UsesClass;
 use PhpUnit\Framework\TestCase;
-use Ruvos\Blog\Database\EventStore;
+use Ruvos\Blog\DomainObject\AbstractDomainObject;
+use Ruvos\Blog\DomainObject\AbstractEvent;
+use Ruvos\Blog\DomainObject\Post\Event\PostCreatedEvent;
 use Ruvos\Blog\DomainObject\Post\PostRepository;
 use Ruvos\Blog\DomainObject\Post\Post;
+use Ruvos\Blog\Utils\DefaultUuidProvider;
 
 #[CoversClass(PostRepository::class)]
+#[UsesClass(AbstractDomainObject::class)]
+#[UsesClass(AbstractEvent::class)]
+#[UsesClass(PostCreatedEvent::class)]
+#[UsesClass(Post::class)]
+#[UsesClass(DefaultUuidProvider::class)]
 final class PostRepositoryTest extends TestCase
 {
 
-	private EventStore $eventStoreMock;
+	private EventStoreMock $eventStoreMock;
+	private PostRepository $postRepository;
 
 	protected function setUp(): void
 	{
 		$this->eventStoreMock = new EventStoreMock();
+		$this->postRepository = new PostRepository($this->eventStoreMock);
 	}
 
 	public function testPostRepositoryHasCorrectClass(): void
 	{
-		$repository = new PostRepository($this->eventStoreMock);
+		$this->assertSame(Post::class, $this->postRepository->getRelevantDomainObject()); 
+	}
 
-		$this->assertSame(Post::class, $repository->getRelevantDomainObject()); 
+	public function testLoadAllReturns2Posts(): void
+	{
+		$createdFirstPostEvent = PostCreatedEvent::create('firstPost');
+		$createdSecondPostEvent = PostCreatedEvent::create('secondPost');
+
+		$this->eventStoreMock->events = [$createdFirstPostEvent, $createdSecondPostEvent];
+
+		$posts = $this->postRepository->loadAll();
+		$this->assertCount(2, $posts);
+
+		$this->assertInstanceOf(Post::class, $posts[0]);
+		$this->assertInstanceOf(Post::class, $posts[1]);
 	}
 }
