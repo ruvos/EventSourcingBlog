3cd85581deff37bbc7bee1e0712e4d43f37cec9d
Ruwen Katschek <ruwen@acer-laptop-ruwe.ruwen>
2025-06-20 17:00:48 +0200

    feat: Add restoring functionality

--- src/DomainObject/Content/Event/TextContentCreatedEvent.php
+++ src/DomainObject/Content/Event/TextContentCreatedEvent.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ruvos\Blog\DomainObject\Content\Event;
 
+use DateTimeImmutable;
 use Ruvos\Blog\DomainObject\AbstractEvent;
 use Ruvos\Blog\Utils\DefaultDateTimeProvider;
 use Ruvos\Blog\Utils\DefaultUuidProvider;
@@ -34,5 +35,17 @@ final readonly class TextContentCreatedEvent extends AbstractEvent
         $eventId = DefaultUuidProvider::v4();
 
         return new self($eventId, $correlationId, $createdAt, $content);
+    }
+
+    public static function fromJson(string $payload): AbstractEvent
+    {
+        $data = json_decode($payload, true);
+
+        return new self(
+            $data['eventId'],
+            $data['correlationId'],
+            new DateTimeImmutable($data['createdAt']['date'], new \DateTimeZone($data['createdAt']['timezone'])),
+            $data['content']
+        );
     }
 }
