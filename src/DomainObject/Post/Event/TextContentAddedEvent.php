d0250863ceb34112e427cc37092fd74978dc8bdf
Ruwen Katschek <ruwen@acer-laptop-ruwe.ruwen>
2025-06-20 14:48:50 +0200

    feat: Add static construction

--- src/DomainObject/Post/Event/TextContentAddedEvent.php
+++ src/DomainObject/Post/Event/TextContentAddedEvent.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace Ruvos\Blog\DomainObject\Post\Event;
 
 use Ruvos\Blog\DomainObject\AbstractEvent;
+use Ruvos\Blog\DomainObject\Event;
+use Ruvos\Blog\DomainObject\Post\State;
+use Ruvos\Blog\Utils\DefaultDateTimeProvider;
+use Ruvos\Blog\Utils\DefaultUuidProvider;
 
 final readonly class TextContentAddedEvent extends AbstractEvent
 {
@@ -22,5 +26,14 @@ final readonly class TextContentAddedEvent extends AbstractEvent
         public string $textContentId
     ) {
         parent::__construct($correlationId, $eventId, $createdAt);
+    }
+
+    public static function create(string $textContentId): Event
+    {
+        $createdAt = DefaultDateTimeProvider::now();
+        $correlationId = DefaultUuidProvider::v4();
+        $eventId = DefaultUuidProvider::v4();
+
+        return new self($eventId, $correlationId, $createdAt, $textContentId);
     }
 }
