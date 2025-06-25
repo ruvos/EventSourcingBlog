b57d77e89f5fa749125c07859c40362c98c21dd1
Ruwen Katschek <ruwen@acer-laptop-ruwe.ruwen>
2025-06-25 12:19:33 +0200

    refactor: Set relevant events during construction, since it is not needed during runtime

--- src/DomainObject/Post/PostRepository.php
+++ src/DomainObject/Post/PostRepository.php
@@ -4,23 +4,27 @@ declare(strict_types=1);
 
 namespace Ruvos\Blog\DomainObject\Post;
 
+use Ruvos\Blog\Database\EventStore;
 use Ruvos\Blog\DomainObject\AbstractRepository;
 use Ruvos\Blog\DomainObject\DomainObjectRepository;
 use Ruvos\Blog\DomainObject\Post\Event\PostCreatedEvent;
 use Ruvos\Blog\DomainObject\Post\Event\TextContentAddedEvent;
 
-final readonly class PostRepository extends AbstractRepository implements DomainObjectRepository
+final class PostRepository extends AbstractRepository implements DomainObjectRepository
 {
+    public function __construct(EventStore $eventStore)
+    {
+        parent::__construct($eventStore);
+        $this->setRelevantEvents(
+            [
+                PostCreatedEvent::class,
+                TextContentAddedEvent::class
+            ]
+        );
+    }
+
     public function getRelevantDomainObject(): string
     {
         return Post::class;
-    }
-
-    protected function getRelevantEvents(): array
-    {
-        return [
-            PostCreatedEvent::class,
-            TextContentAddedEvent::class
-        ];
     }
 }
