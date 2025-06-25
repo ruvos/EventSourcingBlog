e86fa573ed25feaf01e8b2863d58d0d982d6ca1e
Ruwen Katschek <ruwen@acer-laptop-ruwe.ruwen>
2025-06-25 12:26:20 +0200

    feat: Load all posttitle and display them on the entry file

--- public/index.php
+++ public/index.php
@@ -1,9 +1,20 @@
 <?php
 
+use Ruvos\Blog\DomainObject\Post\PostRepository;
+use Ruvos\Blog\DomainObject\Post\Post;
 require __DIR__ . '/../vendor/autoload.php';
 
 \Ruvos\Blog\Kernel::boot();
 
 $container = \Ruvos\Blog\Kernel::getContainer();
 
-echo("Hier entsteht ein Eventsourcing Blog").PHP_EOL;
+/** @var PostRepository $postRepository */
+$postRepository = $container->get(PostRepository::class);
+
+$posts = $postRepository->loadAll();
+
+/** @var Post $post  */
+foreach($posts as $post)
+{
+    echo($post->getTitle());PHP_EOL;
+}
