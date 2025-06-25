f676f84eaa1ee11cc33b08d093aad5fddfe30867
Ruwen Katschek <ruwen@acer-laptop-ruwe.ruwen>
2025-06-25 12:14:47 +0200

    feat: Add postrepository to services and set absolute path for db file

--- config/services/services.php
+++ config/services/services.php
@@ -4,13 +4,23 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Symfony\Component\DependencyInjection\Reference;
+
 return static function (ContainerConfigurator $container): void {
+
+    $dbDir = __DIR__.'/../';
+
     $container->parameters()
-        ->set('database.dsn', 'sqlite:./../events.db')
+        ->set('database.dsn', 'sqlite:'.$dbDir.'events.db')
     ;
 
     $services = $container->services();
     $services->set(\Ruvos\Blog\Database\SqLiteEventStore::class)
         ->args(['%database.dsn%'])
     ;
+
+    $services->set(\Ruvos\Blog\DomainObject\Post\PostRepository::class)
+    ->args([
+        new Reference(\Ruvos\Blog\Database\SqLiteEventStore::class)
+    ])->public();
 };
